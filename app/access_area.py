08fabeb2950ad60a051de2e622a08b283eb8cb50
nurfachridaffa17 <nurfachridaffa17@gmail.com>
2023-08-18 10:35:31 +0000

    fear : add featured crud for access area

---
+++ app/access_area.py
@@ -0,0 +1,68 @@
+from .models import db,M_AccessArea
+from flask import request, jsonify
+import datetime
+
+def create_access_area():
+    now = datetime.datetime.now()
+    new_access_area = M_AccessArea(
+        name = request.form.get('name'),
+        created_at = now,
+        is_active = 1,
+    )
+
+    db.session.add(new_access_area)
+    db.session.commit()
+
+    return jsonify({'message': 'New access area created!'}), 200
+
+def get_all_access_area():
+    access_areas = M_AccessArea.query.all()
+    if not access_areas:
+        return jsonify({'message': 'No access area found!'}), 404
+
+    output = []
+    for access_area in access_areas:
+        access_area_data = {}
+        access_area_data['id'] = access_area.id
+        access_area_data['name'] = access_area.name
+        access_area_data['created_at'] = access_area.created_at
+        access_area_data['is_active'] = access_area.is_active
+        output.append(access_area_data)
+
+    return jsonify({'access_areas': output}), 200
+
+def get_access_area_by_id(id):
+    access_area = M_AccessArea.query.filter_by(id=id).first()
+    if not access_area:
+        return jsonify({'message': 'No access area found!'}), 404
+
+    access_area_data = {}
+    access_area_data['id'] = access_area.id
+    access_area_data['name'] = access_area.name
+    access_area_data['created_at'] = access_area.created_at
+    access_area_data['is_active'] = access_area.is_active
+
+    return jsonify({'access_area': access_area_data}), 200
+
+def update_access_area(id):
+    access_area = M_AccessArea.query.filter_by(id=id).first()
+    if not access_area:
+        return jsonify({'message': 'No access area found!'}), 404
+    
+    try:
+        access_area.name = request.form.get('name')
+        access_area.is_active = request.form.get('is_active')
+        db.session.commit()
+        return jsonify({'message': 'Access area updated!'}), 200
+    except:
+        return jsonify({'message': 'Access area not updated!'}), 500
+
+def delete_access_area(id):
+    access_area = M_AccessArea.query.filter_by(id=id).first()
+    if not access_area:
+        return jsonify({'message': 'No access area found!'}), 404
+
+    db.session.delete(access_area)
+    db.session.commit()
+
+    return jsonify({'message': 'Access area deleted!'}), 200
--- app/views.py
+++ app/views.py
@@ -7,6 +7,7 @@ from .user import create_user, get_all_user, get_user_by_id, update_user, delete
 from .masterCard import create_card, get_all_card, get_card_by_id, update_card, delete_card
 from .usertype import create_usertype, get_all_usertype, get_usertype_by_id, update_usertype, delete_usertype
 from flask_login import login_required
+from .access_area import create_access_area, get_all_access_area, get_access_area_by_id, update_access_area, delete_access_area
 
 @app.route('/api/v1/user/create', methods=['POST'])
 def user_route():
@@ -85,4 +86,30 @@ def delete_usertype_route():
     if id is None:
         return jsonify({'message': 'Missing usertype ID parameter'}), 400
     return delete_usertype(id=id)
+
+@app.route('/api/v1/access_area', methods=['POST'])
+def get_access_area():
+    if request.method == 'POST':
+        return create_access_area()
+
+@app.route('/api/v1/access_area/all', methods=['GET'])
+def get_all_access_area_route():
+    return get_all_access_area()
+
+@app.route('/api/v1/access_area', methods=['GET','PUT'])
+def get_update_access_area():
+    id = request.args.get('id')
+    if id is None:
+        return jsonify({'message': 'Missing access_area ID parameter'}), 400
+    if request.method == 'GET':
+        return get_access_area_by_id(id)
+    elif request.method == 'PUT':
+        return update_access_area(id=id)
+
+@app.route('/api/v1/access_area/delete', methods=['DELETE'])
+def delete_access_area_route():
+    id = request.args.get('id')
+    if id is None:
+        return jsonify({'message': 'Missing access_area ID parameter'}), 400
+    return delete_access_area(id=id)
 

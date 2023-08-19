faf7a7d18646f30ca1e17d75ce4ed0ec278dc4ec
nurfachridaffa17 <nurfachridaffa17@gmail.com>
2023-08-19 06:29:46 +0000

    fear : add mail server smtp

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
 
 db = SQLAlchemy()
 app = Flask(__name__)
+mail = Mail()
 
 def create_app():
     app.config.from_object('config.Config')
     db.init_app(app)
+    mail.init_app(app)
     from . import views
     return app
